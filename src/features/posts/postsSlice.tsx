69dfb035c5ab980d4a558b2c5fb6c3e2af0bc809
Zac Cooper <zacooper5g@outlook.com>
2022-10-24 16:25:59 +1100

    added date-fns for date display

--- src/features/posts/postsSlice.tsx
+++ src/features/posts/postsSlice.tsx
@@ -1,4 +1,5 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
 
 // interface postsStateProps {
 //     id: string
@@ -10,7 +11,8 @@ const initialState = [
     {
         id: '1',
         title: 'Learning Redux Toolkit',
-        content: 'I\'ve heard good things'
+        content: 'I\'ve heard good things',
+        date: sub(new Date(), {minutes: 10}).toISOString()
     }
 ];
 
@@ -29,6 +31,7 @@ const postsSlice: any = createSlice({
                         id: nanoid(),
                         title,
                         content,
+                        date: new Date().toISOString(),
                         userId,
                     }
                 }
