6893be609ccc062ca203af05f872045e25e6f03e
Zac Cooper <zacooper5g@outlook.com>
2022-10-24 16:41:35 +1100

    added reactions state

--- src/features/posts/postsSlice.tsx
+++ src/features/posts/postsSlice.tsx
@@ -12,7 +12,11 @@ const initialState = [
         id: '1',
         title: 'Learning Redux Toolkit',
         content: 'I\'ve heard good things',
-        date: sub(new Date(), {minutes: 10}).toISOString()
+        date: sub(new Date(), {minutes: 10}).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            heart: 0,
+        }
     }
 ];
 
@@ -33,10 +37,21 @@ const postsSlice: any = createSlice({
                         content,
                         date: new Date().toISOString(),
                         userId,
+                        reactions: {
+                            thumbsUp: 0,
+                            heart: 0,
+                        }
                     }
                 }
             }
-        }
+        },
+        reactionAdded (state, action) {
+            const { postId, reaction } = action.payload;
+            const existingPost: any = state.find((post: any) => post.id === postId);
+            if (existingPost) {
+                existingPost.reactions[reaction]++
+            };
+        },
     }
 });
 
@@ -44,6 +59,6 @@ export const selectAllPosts = (state: any) => {
     return state.posts;
 };
 
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
